e7916cb1b426acb4febc8ef85e46aed6b51dc173
dr0id007 <nullvoid.nv@gmail.com>
2020-04-30 17:59:08 +0530

    div added

--- src/comp/UIComponents/Cards.js
+++ src/comp/UIComponents/Cards.js
@@ -4,15 +4,17 @@ import "../../style/Card.css";
 const Cards = (props) => {
   console.log("datsdasda:-", props.data);
   return (
-    <div className="card shadow round col-4">
-      <h5 class="card-title">{props.data.name}</h5>
-      <p class="card-text">
-        From:- {props.data.startTime} <br />
-        To:- {props.data.endTime}
-      </p>
-      <a href={props.data.url} class="">
-        Go To Contest
-      </a>
+    <div className="col-3 mb-2">
+      <div className="card shadow round w-100 h-100">
+        <h5 class="card-title mt-2">{props.data.name}</h5>
+        <p class="card-text">
+          From:- {props.data.startTime} <br />
+          To:- {props.data.endTime}
+        </p>
+        <a href={props.data.url} class="">
+          Go To Contest
+        </a>
+      </div>
     </div>
   );
 };
